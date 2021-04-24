6fc852bf8aea78e0b7f2c28b0e2867378b010eaf
dustin <dtc97bh@gmail.com>
2021-04-24 02:15:35 -0600

    add all files

---
+++ Frontend/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import store from './store'
+import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Font Awesome stuff
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  store,
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
