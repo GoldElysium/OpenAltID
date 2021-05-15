0341fe00113abb2db4283f9ea36cc0bd79bf1901
Irony4D <dtc97bh@gmail.com>
2021-05-15 01:17:22 -0600

    all the auth routes added now

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 
-const express        = require('express');
-const session        = require('express-session');
-const passport       = require('passport');
-const request        = require('request');
-const bodyParser     = require('body-parser');
-const morgan         = require('morgan')('combined');
+const express           = require('express');
+const session           = require('express-session');
+const passport          = require('passport');
+const request           = require('request');
+const bodyParser        = require('body-parser');
+const morgan            = require('morgan')('combined');
 
-const Strategy       = require('passport-twitter').Strategy;
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const TwitchStrategy = require('passport-oauth').OAuth2Strategy;
+const TwitterStrategy   = require('passport-twitter').Strategy;
+const GoogleStrategy    = require('passport-google-oauth').OAuth2Strategy;
+const TwitchStrategy    = require('passport-oauth').OAuth2Strategy;
+const DiscordStrategy   = require('passport-discord').Strategy;
+const RedditStrategy    = require('passport-reddit').Strategy;
 
 let trustProxy = false;
 if (process.env.DYNO) {
@@ -28,9 +30,9 @@ app.use(session({secret: process.env['SECRET'], resave: true, saveUninitialized:
 
 
 // Twitter Passport
-passport.use(new Strategy({
-        consumerKey: process.env['TWITTER_CONSUMER_KEY'],
-        consumerSecret: process.env['TWITTER_CONSUMER_SECRET'],
+passport.use(new TwitterStrategy({
+        consumerKey: process.env.TWITTER_CLIENT_ID,
+        consumerSecret: process.env.TWITTER_CLIENT_SECRET,
         callbackURL: '/auth/twitter/callback',
         proxy: trustProxy
     },
@@ -43,8 +45,8 @@ passport.use(new Strategy({
 
 // Google Passport
 passport.use(new GoogleStrategy({
-        clientID: process.env['GOOGLE_CLIENT_ID'],
-        clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "/auth/google/callback"
     },
     function (accessToken, refreshToken, profile, done) {
@@ -62,7 +64,7 @@ TwitchStrategy.prototype.userProfile = function (accessToken, done) {
         url: 'https://api.twitch.tv/helix/users',
         method: 'GET',
         headers: {
-            'Client-ID': process.env['TWITCH_CLIENT_ID'],
+            'Client-ID': process.env.TWITCH_CLIENT_ID,
             'Accept': 'application/vnd.twitchtv.v5+json',
             'Authorization': 'Bearer ' + accessToken
         }
@@ -79,20 +81,53 @@ TwitchStrategy.prototype.userProfile = function (accessToken, done) {
 passport.use('twitch', new TwitchStrategy({
         authorizationURL: 'https://id.twitch.tv/oauth2/authorize',
         tokenURL: 'https://id.twitch.tv/oauth2/token',
-        clientID: process.env['TWITCH_CLIENT_ID'],
-        clientSecret: process.env['TWITCH_SECRET'],
+        clientID: process.env.TWITCH_CLIENT_ID,
+        clientSecret: process.env.TWITCH_CLIENT_SECRET,
         callbackURL: "/auth/google/callback",
         state: true
     },
     function (accessToken, refreshToken, profile, done) {
-        profile.accessToken = accessToken;
-        profile.refreshToken = refreshToken;
-
-        done(null, profile);
+        console.log("Access Token:" + accessToken)
+        console.log("Refresh Token:" + refreshToken)
+        console.log("Profile" + profile.toString())
+        return done(null, profile);
     }
 ));
 
 
+// Reddit Passport
+passport.use(new RedditStrategy({
+        clientID: process.env.REDDIT_CLIENT_ID,
+        clientSecret: process.env.REDDIT_CLIENT_SECRET,
+        callbackURL: "/auth/reddit/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        console.log("Access Token:" + accessToken)
+        console.log("Refresh Token:" + refreshToken)
+        console.log("Profile" + profile.toString())
+        return done(null, profile);
+    }
+));
+
+
+// Discord Passport
+passport.use(new DiscordStrategy({
+        clientID: process.env.DISCORD_CLIENT_ID,
+        clientSecret: process.env.DISCORD_CLIENT_SECRET,
+        callbackURL: '/auth/discord/callback',
+        scope: ['identify', 'guilds'],
+        state: true
+    },
+    function(accessToken, refreshToken, profile, done) {
+        console.log("Access Token:" + accessToken)
+        console.log("Refresh Token:" + refreshToken)
+        console.log("Profile" + profile.toString())
+        return done(null, profile);
+    }));
+
+
+
+// Serialize Stuff
 passport.serializeUser(function (user, done) {
     done(null, user);
 });
@@ -110,30 +145,67 @@ app.get('/',
 
 app.get('/login',
     function (req, res) {
-        console.log('ENV');
-        console.log(process.env);
-        console.log('Headers:');
-        console.log(req.headers)
-        res.json({"Page": "Login Page"})
+        res.redirect('/auth/discord')
     });
 
+// Google (tested) /////////////////////////////////////////////////////////////////////////////////////////////////////
 app.get('/auth/google',
-    passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/plus.login']}));
+    passport.authorize('google', {scope: ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/plus.login']
+    }));
 
 app.get('/auth/google/callback',
-    passport.authenticate('google', {failureRedirect: '/auth/google'}),
+    passport.authorize('google', {failureRedirect: '/login'}),
     function (req, res) {
-        res.redirect('/');
+        // Successful
+        console.log("Successful")
+        res.json({"Success": true})
     });
 
+// Twitch (redirect mismatch) //////////////////////////////////////////////////////////////////////////////////////////
 app.get('/auth/twitch',
-    passport.authenticate('twitch', {scope: 'user_read'}));
+    passport.authorize('twitch', {
+        scope: 'user_read',
+        successRedirect: '/',
+        failureRedirect: '/'
+    }));
 
 app.get('/auth/twitch/callback',
-    passport.authenticate('twitch', {successRedirect: '/', failureRedirect: '/'}),
+    passport.authorize('twitch', {failureRedirect: '/login'}),
     function (req, res) {
-        res.redirect('/');
+        // Successful
+        console.log("Successful")
+        res.json({"Success": true})
     });
+
+// Discord (redirect mismatch) /////////////////////////////////////////////////////////////////////////////////////////
+app.get('/auth/discord',
+    passport.authenticate('discord'));
+
+app.get('/auth/discord/callback',
+    passport.authenticate('discord'),
+    function(req, res) {
+        // Successful
+        console.log("Successful")
+        res.json({"Success": true})
+    });
+
+// Reddit //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+app.get('/auth/reddit', function (req, res, next) {
+    req.session.state = crypto.randomBytes(32).toString('hex');
+    passport.authorize('reddit', {
+        state: req.session.state
+    });
+});
+
+app.get('/auth/reddit/callback', function (req, res, next) {
+    if (req.query.state === req.session.state) {
+        passport.authorize('reddit', {
+            successRedirect: '/'
+        })(req, res, next);
+    } else {
+        next(new Error(403))
+    }
+});
 
 app.get('/profile',
     require('connect-ensure-login').ensureLoggedIn(),
