54bfbd0dd510469e50ecb220bfb87113bfc53836
Irony4D <dtc97bh@gmail.com>
2021-06-04 00:07:55 -0600

    ran prettier

--- Backend/database/models/UserModel.js
+++ Backend/database/models/UserModel.js
@@ -14,6 +14,7 @@ let userSchema = new mongoose.Schema({
     verified: Boolean,
     accessToken: String,
     avatar: String,
+    verified: Boolean,
     connections: [connectionSchema],
 })
 
--- Backend/routes/user/UserLogin.js
+++ Backend/routes/user/UserLogin.js
@@ -1,49 +1,47 @@
-const express = require('express')
-const axios = require('axios')
-const { verifyUser } = require("./UserFunctions");
-const { getAccountAges } = require("./UserFunctions");
-const { getUserConnectionIDs } = require("./UserFunctions");
+const express = require('express');
+const axios = require('axios');
+const { verifyUser } = require('./UserFunctions');
+const { getAccountAges } = require('./UserFunctions');
+const { getUserConnectionIDs } = require('./UserFunctions');
 
-let router = express.Router()
+let router = express.Router();
 
 // This is just a synonym of auth/discord
 router.get('/login', function (req, res) {
-    res.redirect('http://localhost:8080/auth/discord')
-})
+    res.redirect('http://localhost:8080/auth/discord');
+});
 
 // Just destroys the session and goes back /
 router.get('/logout', function (req, res) {
     req.session.destroy(function (err) {
-        res.redirect('/')
-    })
-})
-
+        res.redirect('/');
+    });
+});
 
 router.get('/verify-accounts', async (req, res) => {
     if (!req.user) {
         return res.status(401).send({
             message: 'No user in session, you must login first.',
-        })
+        });
     } else {
         try {
             let accounts = await getUserConnectionIDs(req.user);
-            accounts = await getAccountAges(accounts)
+            accounts = await getAccountAges(accounts);
 
             // Todo Actually pass in the real server ID
-            let verified = await verifyUser(accounts, 123456789 ,req.user)
+            let verified = await verifyUser(accounts, 123456789, req.user);
 
             return res.send({
                 verified: verified,
-            })
-
+            });
         } catch (e) {
-            console.log(e)
+            console.log(e);
             res.status(500).send({
-                message: "Error occurred while fetching account info."
-            })
+                message: 'Error occurred while fetching account info.',
+            });
         }
-        res.send('Done.')
+        res.send('Done.');
     }
-})
+});
 
-module.exports = router
+module.exports = router;
